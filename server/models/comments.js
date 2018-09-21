c9cfaf6a9d26e57146c267a2628bd397cb79849c
KidneyFlower <753475777@qq.com>
2018-09-21 20:48:48 +0800

    Add model for comments

---
+++ server/models/comments.js
@@ -0,0 +1,38 @@
+module.exports = (sequelize, DataTypes) => {
+  return sequelize.define(
+    'comments',
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      user_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+      },
+      user_name: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      user_avatar: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      dish_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+      },
+      comment: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      rate: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+      },
+      created_at: DataTypes.DATE,
+      updated_at: DataTypes.DATE
+    }, { tableName: 'comments' }
+  )
+}
