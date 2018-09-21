554bebd596888d2350536dc8dbcb3c2803378072
KidneyFlower <753475777@qq.com>
2018-09-21 20:48:23 +0800

    Add model for dishes

---
+++ server/models/dishes.js
@@ -0,0 +1,28 @@
+module.exports = (sequelize, DataTypes) => {
+  return sequelize.define(
+    'dishes',
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      shop_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+      },
+      shop_name: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      rate: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+      }
+    }, { tableName: 'dishes' }
+  )
+}
