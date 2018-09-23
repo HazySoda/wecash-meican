4cfbe2fe611fd88cf9ac8dce81571eab154e797d
KidneyFlower <753475777@qq.com>
2018-09-23 11:42:28 +0800

    Add API for create a new dish

--- server/routes/dishes.js
+++ server/routes/dishes.js
@@ -1,4 +1,5 @@
 const Joi = require('joi')
+const Boom = require('boom')
 const { paginationDefine } = require('../utils/router-helper')
 const models = require('../models')
 
@@ -31,6 +32,48 @@ module.exports = [
       })
       req.total = total
       return results
+    }
+  },
+  {
+    method: 'POST',
+    path: `/${GROUP_NAME}`,
+    config: {
+      tags: ['api', GROUP_NAME],
+      description: '新增菜品',
+      notes: '新增菜品',
+      auth: false,
+      validate: {
+        payload: {
+          name: Joi.string().required().description('商家名称'),
+          shopId: Joi.number().integer().min(1).required().description('商家类型'),
+          rate: Joi.number().min(0).max(5).default(0).description('分数')
+        }
+      },
+      handler: async (req, h) => {
+        const { name, shopId, rate } = req.payload
+        const shop = await models.shops.findOne({
+          where: {
+            id: shopId
+          }
+        })
+        if (!shop) {
+          throw Boom.badRequest('商家不存在')
+        }
+        await models.dishes.create({
+          name,
+          shop_id: shopId,
+          shop_name: shop.name,
+          rate
+        })
+        await shop.update({
+          dishes_count: shop.dishes_count + 1
+        })
+        return {
+          statusCode: 200,
+          error: null,
+          message: '操作成功!'
+        }
+      }
     }
   }
 ]
