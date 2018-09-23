058b04a79a45f7dfb6af5314aa2eabfaaf6f8811
KidneyFlower <753475777@qq.com>
2018-09-23 11:09:30 +0800

    Add API for create a new shop

--- server/routes/shops.js
+++ server/routes/shops.js
@@ -1,3 +1,4 @@
+const Joi = require('joi')
 const { paginationDefine } = require('../utils/router-helper')
 const models = require('../models')
 
@@ -9,8 +10,8 @@ module.exports = [
     path: `/${GROUP_NAME}`,
     config: {
       tags: ['api', GROUP_NAME],
-      description: '获取商品列表',
-      notes: '获取商品列表',
+      description: '获取商家列表',
+      notes: '获取商家列表',
       auth: false,
       validate: {
         query: {
@@ -26,6 +27,38 @@ module.exports = [
       })
       req.total = total
       return results
+    }
+  },
+  {
+    method: 'POST',
+    path: `/${GROUP_NAME}`,
+    config: {
+      tags: ['api', GROUP_NAME],
+      description: '新增商家',
+      notes: '新增商家',
+      auth: false,
+      validate: {
+        payload: {
+          name: Joi.string().required().description('商家名称'),
+          type: Joi.number().integer().min(1).max(2).required().description('商家类型'),
+          dishesCount: Joi.number().integer().min(0).default(0).description('菜品数量'),
+          rate: Joi.number().min(0).max(5).default(0).description('分数')
+        }
+      },
+      handler: async (req, h) => {
+        const { name, type, dishesCount, rate } = req.payload
+        await models.shops.create({
+          name,
+          type,
+          dishes_count: dishesCount,
+          rate
+        })
+        return {
+          statusCode: 200,
+          error: null,
+          message: '操作成功!'
+        }
+      }
     }
   }
 ]
