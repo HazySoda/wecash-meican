d682d3f7afc0b9c82c5ef95b574f0be26f0817f9
KidneyFlower <753475777@qq.com>
2018-09-23 10:57:13 +0800

    Add API for create a new comment

--- server/routes/comments.js
+++ server/routes/comments.js
@@ -32,5 +32,41 @@ module.exports = [
       req.total = total
       return results
     }
+  },
+  {
+    method: 'POST',
+    path: `/${GROUP_NAME}`,
+    config: {
+      tags: ['api', GROUP_NAME],
+      description: '新增评价',
+      notes: '新增评价',
+      auth: false,
+      validate: {
+        payload: {
+          userId: Joi.number().integer().min(1).required().description('userID'),
+          userName: Joi.string().required().description('userName'),
+          userAvatar: Joi.string().required().description('userAvatar'),
+          dishId: Joi.number().integer().min(1).description('dishId'),
+          comment: Joi.string().required().description('comment'),
+          rate: Joi.number().min(0).max(5).required().description('rate')
+        }
+      }
+    },
+    handler: async (req, h) => {
+      const { userId, userName, userAvatar, dishId, comment, rate } = req.payload
+      await models.comments.create({
+        user_id: userId,
+        user_name: userName,
+        user_avatar: userAvatar,
+        dish_id: dishId,
+        comment,
+        rate
+      })
+      return {
+        statusCode: 200,
+        error: null,
+        message: '操作成功!'
+      }
+    }
   }
 ]
