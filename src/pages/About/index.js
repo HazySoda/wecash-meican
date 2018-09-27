52a0a7a792ccbde30606da6200691d7a4542dd8a
KidneyFlower <753475777@qq.com>
2018-09-28 00:26:45 +0800

    Edit text in About page

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -23,11 +23,12 @@ class About extends Component {
         <View className='panel'>
           <View className='panel__title'>关于</View>
           <View className='panel__content padding'>
-            <View class='content'>民以食为天。</View>
-            <View class='content'>在闪银工作餐的众多选择中，我们可能都迷茫过。</View>
-            <View class='content'>希望这个小程序可以帮每一个人找到自己的菜。</View>
-            <View class='content'>如果你觉得这对你有些许帮助，点击图片长按扫码。</View>
-            <View class='content'>请我喝杯咖啡吧，Respect~</View>
+            <View class='content'>中秋三天加上一些业余时间写了这个小程序，</View>
+            <View class='content'>万万没想到的是它还没上线，美餐先下岗了。</View>
+            <View class='content'>不过写代码的过程还是很快乐的，美餐的晚餐也还在，</View>
+            <View class='content'>所以还是决定发布了，希望它可以帮助到加班的闪宝们。</View>
+            <View class='content'>如果它帮你找到了属于你的菜，可以请我喝杯咖啡~</View>
+            <View class='content'>Work Hard && Eat Harder</View>
             <Image class='image' src={this.state.imgUrl} onClick={this.handleImageClick} />
           </View>
         </View>
